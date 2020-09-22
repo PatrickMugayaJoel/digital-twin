7e933e3fa4196c54112d73c0c519e2fffa1abd46
Joel Mugaya <josean.patrick11@gmail.com>
2020-09-22 19:30:19 +0300

    Adds a router

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/">
+          <App />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
