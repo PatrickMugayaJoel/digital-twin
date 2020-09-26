ae68f01888208747fb7abe6727c267b5c9ce8b20
Joel Mugaya <josean.patrick11@gmail.com>
2020-09-26 19:24:15 +0300

    cesium setup

--- src/components/CesiumView/CesiumView.js
+++ src/components/CesiumView/CesiumView.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './CesiumView.css';
+import { Viewer, Entity } from "resium";
+import { Cartesian3 } from "cesium";
 
 function CesiumView() {
   return (
-    <div className="cesium_field"></div>
+  <div className="cesium_field">
+      <Viewer>
+        <Entity
+          description="test"
+          name="tokyo"
+          point={{ pixelSize: 10 }}
+          position={Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
+        />
+      </Viewer>
+    </div>
   );
 }
 
