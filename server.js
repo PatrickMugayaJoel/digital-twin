427984b3dbdf545684172d60d005f3242c11194c
Joel Mugaya <josean.patrick11@gmail.com>
2020-10-10 12:19:23 +0300

    updating server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const express = require('express');
 const path = require('path');
+const  morgan = require('morgan');
+const fs = require('fs');
 
 const app = express();
 
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'server.log'), { flags: 'a' })
+
 app.use(express.static('build'));
+app.use(morgan('combined', { stream: accessLogStream }));
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = 80;
 
-app.listen(PORT);
-
-console.log("server listing on port ", PORT);
+app.listen(PORT, () => {
+  console.log(`App is listening at port: ${PORT}`);
+});
