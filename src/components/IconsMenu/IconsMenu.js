23ee6169023d3183cc0ac14def5cd18d5eeb1efb
Joel Mugaya <josean.patrick11@gmail.com>
2020-10-17 07:41:07 +0300

    adds an icon

--- src/components/IconsMenu/IconsMenu.js
+++ src/components/IconsMenu/IconsMenu.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './IconsMenu.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGrinTongueWink } from '@fortawesome/free-solid-svg-icons'
+import logo from '../../logo.png';
 
 function IconsMenu() {
   return (
     <div className="icons_menu grid-item">
       {/* <FontAwesomeIcon icon={faGrinTongueWink} /> */}
+      <div className="logo home-icon">
+        <img src={logo} alt="logo" width="50" height="50" /> 
+      </div>
     </div>
   );
 }
