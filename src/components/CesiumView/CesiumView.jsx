30396d6b918f51db553bb9528df4a944bf8f5b27
Joel Mugaya <josean.patrick11@gmail.com>
2020-10-18 10:23:16 +0300

    refactor

--- src/components/CesiumView/CesiumView.jsx
+++ src/components/CesiumView/CesiumView.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import './CesiumView.scss';
-import { Viewer, Entity } from "resium";
-import { Cartesian3, Ion } from "cesium";
-// import TheModal from "../sandbox/Modal"
+import * as Cesium from 'cesium';
+import "cesium/Build/Cesium/Widgets/widgets.css";
+
 
 function CesiumView() {
-  Ion.defaultAccessToken = process.env.REACT_APP_CESIUM_TOKEN;
-  return (
-    <div className="cesium_field">
-      {/* <TheModal/> */}
-      {/* <Viewer>
-        <Entity
-          description="test"
-          name="tokyo"
-          point={{ pixelSize: 10 }}
-          position={Cartesian3.fromDegrees(139.767052, 35.681167, 100)}
-        />
-      </Viewer> */}
-    </div>
-  );
+  const cesiumContainer = useRef();
+
+  // Your access token can be found at: https://cesium.com/ion/tokens.
+  Cesium.Ion.defaultAccessToken = process.env.REACT_APP_CESIUM_TOKEN;
+
+  useEffect(() => {
+    // Initialize the Cesium Viewer in the HTML element with the "cesiumContainer" ID.
+    const viewer = new Cesium.Viewer(cesiumContainer.current, {
+      terrainProvider: Cesium.createWorldTerrain()
+    });
+
+    // Add Cesium OSM Buildings, a global 3D buildings layer.
+    const buildingTileset = viewer.scene.primitives.add(Cesium.createOsmBuildings());   
+    // Fly the camera to San Francisco at the given longitude, latitude, and height.
+    // viewer.camera.flyTo({
+    //   destination : Cesium.Cartesian3.fromDegrees(-122.4175, 37.655, 400),
+    //   orientation : {
+    //     heading : Cesium.Math.toRadians(0.0),
+    //     pitch : Cesium.Math.toRadians(-15.0),
+    //   }
+    // });
+  });
+  return <div className="cesium_field" ref={cesiumContainer}></div>;
 }
 
 export default CesiumView;
