907262e75a9c08e7e22cc4b725611020fea7c77b
Joel Mugaya <josean.patrick11@gmail.com>
2020-11-10 21:01:27 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV !== 'production') {
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['target']
+    whitelist: ['auth']
 }
 
 const persistedReducer = persistReducer(persistConfig, reducer)
